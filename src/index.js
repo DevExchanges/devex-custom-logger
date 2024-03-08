a48f4a55aca120e184de7011cf5ca1b4b926d376
DevExchanges <wandered.vip.007@gmail.com>
2024-03-08 09:01:33 +0700

    index and test

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const log = require('./logger.js');
+
+const logger = log.getLogger();
+
+module.exports = logger;
---
+++ src/test.js
@@ -0,0 +1,4 @@
+const logger = require('./index.js');
+
+logger.info('456');
+logger.debug('456');
